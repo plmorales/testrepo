5e08fcb112ac13dbabf4b154795fe360a4234d0c
Pedro L. Morales <p.morales.rodriguez@accenture.com>
2019-10-27 19:43:31 +0100

    Initial commit

---
+++ testproject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testproject
+//
+//  Created by Morales Rodriguez, P. on 27/10/2019.
+//  Copyright © 2019 mobgen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
